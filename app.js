625953315b50e539d0d149cd40396b88e6e4706c
facundo <facufr2@hotmail.com>
2020-10-21 16:23:01 -0300

    nose que pija pasa

---
+++ app.js
@@ -0,0 +1,14 @@
+
+function login () {
+    var usuario = document.getElementById(usuario).value;
+    var contraseña =document.getElementById(contraseña).value;
+    
+    if (usuario == "facundokpo12" && contraseña == "joacokpo12"){
+        alert ("pasapa")
+    }
+    else {
+        alert("usuario incorrecto");
+    }
+    
+    
+    }
